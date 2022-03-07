ae17d52d90089677709ee29e65257b4487e014ff
myafka <sorry88@bk.ru>
2022-03-07 17:25:25 +0000

    fixed api key

--- getting-started/cs/create-payment-intent.cs
+++ getting-started/cs/create-payment-intent.cs
@@ -2,6 +2,6 @@ var client = new RestClient("https://api.dojo.tech/payment-intents");
 var request = new RestRequest(Method.POST);
 request.AddHeader("content-type", "application/json");
 request.AddHeader("version", "2022-01-03");
-request.AddHeader("Authorization", "YOUR_API_KEY_HERE");
+request.AddHeader("Authorization", "Basic sk_test_m302B3jKTdyIXCOgMJwTrZBlIN4_bFBeuRsuUJqC3QS0w6XR-HTcXT9vfcxPHjw_fPmWFinEitRoGusuxjuM0hTYkO2YQQmalTSRAxX1yQsQWSSLWU3TsJ4ImPRdMKzjP88IJVookJQQ7DgQoD4JK9tbdLbID1h7gNa9d8AtgV24mR0dR1Nwc8rDZxcWRFH_WaOoPfKoaM8TdwZV7PiR3A");
 request.AddParameter("application/json", "{\"captureMode\":\"Auto\",\"amount\":{\"value\":1000,\"currencyCode\":\"GBP\"},\"reference\":\"Order 245\"}}", ParameterType.RequestBody);
 IRestResponse response = client.Execute(request);
