84a37dd68824134530d942fcbc32ae8dd9d1a033
myafka <sorry88@bk.ru>
2022-03-07 17:08:16 +0000

    added python samples

--- manage-payments/curl/refund.sh
+++ manage-payments/curl/refund.sh
@@ -4,4 +4,4 @@ curl --request POST \
   --header 'Authorization: Basic sk_test_m302B3jKTdyIXCOgMJwTrZBlIN4_bFBeuRsuUJqC3QS0w6XR-HTcXT9vfcxPHjw_fPmWFinEitRoGusuxjuM0hTYkO2YQQmalTSRAxX1yQsQWSSLWU3TsJ4ImPRdMKzjP88IJVookJQQ7DgQoD4JK9tbdLbID1h7gNa9d8AtgV24mR0dR1Nwc8rDZxcWRFH_WaOoPfKoaM8TdwZV7PiR3A' \
   --header 'version: 2022-01-03' \
   --header 'idempotencyKey: 656565gfyd65' \
-  --data '{"amount":1000,"refundReason":"Duplicate transaction","notes":"Problems with the site"}'
+  --data '{"amount":1000,"refundReason":"Demo refund","notes":"Duplicate transaction"}'
--- manage-payments/python/capture.py
+++ manage-payments/python/capture.py
@@ -1,1 +1,18 @@
-[TODO]
+import http.client
+
+conn = http.client.HTTPSConnection("api.dojo.tech")
+
+payload = "{\"amount\":1000}"
+
+headers = {
+    'content-type': "application/json",
+    'version': "2022-01-03",
+    'Authorization': "Basic sk_test_m302B3jKTdyIXCOgMJwTrZBlIN4_bFBeuRsuUJqC3QS0w6XR-HTcXT9vfcxPHjw_fPmWFinEitRoGusuxjuM0hTYkO2YQQmalTSRAxX1yQsQWSSLWU3TsJ4ImPRdMKzjP88IJVookJQQ7DgQoD4JK9tbdLbID1h7gNa9d8AtgV24mR0dR1Nwc8rDZxcWRFH_WaOoPfKoaM8TdwZV7PiR3A"
+    }
+
+conn.request("POST", "/payment-intents/pi_pT08VyWG3EC_HQB4NBVliA/captures", payload, headers)
+
+res = conn.getresponse()
+data = res.read()
+
+print(data.decode("utf-8"))
