789cced7738ca83f8fafbe52e92e7d9c5c40532a
myafka <sorry88@bk.ru>
2022-03-07 17:22:43 +0000

    added cs samples

--- manage-payments/cs/reversal.cs
+++ manage-payments/cs/reversal.cs
@@ -1,1 +1,5 @@
-[TODO]
+var client = new RestClient("https://api.dojo.tech/payment-intents/%7BpaymentIntentId%7D/reversal");
+var request = new RestRequest(Method.POST);
+request.AddHeader("version", "2022-01-03");
+request.AddHeader("Authorization", "Basic sk_test_m302B3jKTdyIXCOgMJwTrZBlIN4_bFBeuRsuUJqC3QS0w6XR-HTcXT9vfcxPHjw_fPmWFinEitRoGusuxjuM0hTYkO2YQQmalTSRAxX1yQsQWSSLWU3TsJ4ImPRdMKzjP88IJVookJQQ7DgQoD4JK9tbdLbID1h7gNa9d8AtgV24mR0dR1Nwc8rDZxcWRFH_WaOoPfKoaM8TdwZV7PiR3A");
+IRestResponse response = client.Execute(request);
