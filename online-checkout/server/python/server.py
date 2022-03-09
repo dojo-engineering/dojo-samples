ff11bc167381e1969af0433525f7da0ca4757a56
myafka <sorry88@bk.ru>
2022-03-09 13:20:26 +0000

    added flask

--- online-checkout/server/python/server.py
+++ online-checkout/server/python/server.py
@@ -1,27 +1,47 @@
+from hashlib import new
 import http.client
 import json
+from urllib import response
 
-conn = http.client.HTTPSConnection("api.dojo.tech")
-payload = json.dumps({
-  "amount": {
-    "value": 1000,
-    "currencyCode": "GBP"
-  },
-  "reference": "Order 245"
-})
-headers = {
-    'content-type': "application/json",
-    'version': "2022-01-03",
-    'Authorization': "Basic sk_test_m302B3jKTdyIXCOgMJwTrZBlIN4_bFBeuRsuUJqC3QS0w6XR-HTcXT9vfcxPHjw_fPmWFinEitRoGusuxjuM0hTYkO2YQQmalTSRAxX1yQsQWSSLWU3TsJ4ImPRdMKzjP88IJVookJQQ7DgQoD4JK9tbdLbID1h7gNa9d8AtgV24mR0dR1Nwc8rDZxcWRFH_WaOoPfKoaM8TdwZV7PiR3A" # <-- Change to your secret key
-    }
-conn.request("POST", "/payment-intents/", payload, headers)
-res = conn.getresponse()
-data = res.read()
-resp_data = {}
-resp_data['id'] = json.loads(data)["id"]
-json_data = json.dumps(resp_data)
-resp = app.response_class(
-  response=json_data,
-  mimetype='application/json'
-  )
-return resp
+from flask import Flask, jsonify, request, render_template
+app = Flask(__name__)
+
+@app.route('/checkout', methods=['GET', 'POST'])
+def hello():
+    # POST request
+    if request.method == 'POST':
+        print(request.get_json())  # parse as JSON
+
+        conn = http.client.HTTPSConnection("api.dojo.tech")
+
+        # call post payment-intent 
+        payload = json.dumps({
+          "amount": {
+            "value": 1000,
+            "currencyCode": "GBP"
+          },
+          "reference": "Order 245"
+          })
+        headers = {
+            'content-type': "application/json",
+            'version': "2022-01-03",
+            'Authorization': "Basic sk_test_m302B3jKTdyIXCOgMJwTrZBlIN4_bFBeuRsuUJqC3QS0w6XR-HTcXT9vfcxPHjw_fPmWFinEitRoGusuxjuM0hTYkO2YQQmalTSRAxX1yQsQWSSLWU3TsJ4ImPRdMKzjP88IJVookJQQ7DgQoD4JK9tbdLbID1h7gNa9d8AtgV24mR0dR1Nwc8rDZxcWRFH_WaOoPfKoaM8TdwZV7PiR3A" # <-- Change to your secret key
+            }
+        conn.request("POST", "/payment-intents/", payload, headers)
+
+        # handling the response from POST
+        res = conn.getresponse()
+        data = res.read()
+        resp_data = {}
+        resp_data['id'] = json.loads(data)["id"]
+        json_data = json.dumps(resp_data)
+        resp = app.response_class(
+          response=json_data,
+          mimetype='application/json'
+          )
+        return resp
+
+@app.route('/')
+def test_page():
+  # look inside `templates` and serve `index.html`
+  return render_template('online-checkout-client.html')
