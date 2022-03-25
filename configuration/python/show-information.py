c80020b2503caa57b58c5428f96f5e9f4f2e57af
myafka <sorry88@bk.ru>
2022-03-25 12:38:17 +0300

    change formating

--- configuration/python/show-information.py
+++ configuration/python/show-information.py
@@ -9,50 +9,50 @@ payload = json.dumps({
     },
     "reference": "Order 245",
     "itemLines": [
-                {
-                "id": "item 1",
-                "quantity": 4,
-                "caption": "Dog socks",
-                "amountTotal": {
-                        "value": 400,
-                        "currencyCode": "GBP"
-                    }
-                },
-                {
-                "id": "item 5",
-                "quantity": 1,
-                "caption": "Dog bandana",
-                "amountTotal": {
-                        "value": 600,
-                        "currencyCode": "GBP"
-                    }
-                }
+        {
+            "id": "item 1",
+            "quantity": 4,
+            "caption": "Dog socks",
+            "amountTotal": {
+                "value": 400,
+                "currencyCode": "GBP"
+            }
+        },
+        {
+            "id": "item 5",
+            "quantity": 1,
+            "caption": "Dog bandana",
+            "amountTotal": {
+                "value": 600,
+                "currencyCode": "GBP"
+            }
+        }
     ],
-     "taxLines": [
-                {
-                "id": "vat",
-                "caption": "VAT",
-                "subCaption": "10%",
-                "amountTotal": {
-                        "value": 10,
-                        "currencyCode": "GBP"
-                    }
-                },
-                {
-                "id": "delivery",
-                "caption": "Delivery",
-                "amountTotal": {
-                        "value": 500,
-                        "currencyCode": "GBP"
-                    }
-                }
-            ],
-            "config": {
-                "details": 
-                        {
-                        "showTotal": true
-                        }
-                }
+    "taxLines": [
+        {
+            "id": "vat",
+            "caption": "VAT",
+            "subCaption": "10%",
+            "amountTotal": {
+                "value": 10,
+                "currencyCode": "GBP"
+            }
+        },
+        {
+            "id": "delivery",
+            "caption": "Delivery",
+            "amountTotal": {
+                "value": 500,
+                "currencyCode": "GBP"
+            }
+        }
+    ],
+    "config": {
+        "details":
+        {
+            "showTotal": true
+        }
+    }
 })
 headers = {
     'content-type': "application/json",
