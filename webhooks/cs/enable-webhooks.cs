0441b849b4e8868c584804dfab489c4aa95082ad
myafka <sorry88@bk.ru>
2022-05-09 16:35:00 +0100

    sample for enable webhooks

--- webhooks/cs/enable-webhooks.cs
+++ webhooks/cs/enable-webhooks.cs
@@ -1,1 +1,6 @@
-[TODO]
+var webhooksClient = new Dojo.Net.WebhooksClient(new HttpClient(), new ApiKeyClientAuthorization(“sk_sandbox_c8oLGaI__msxsXbpBDpdtwJEz_eIhfQoKHmedqgZPCdBx59zpKZLSk8OPLT0cZolbeuYJSBvzDVVsYvtpo5RkQ”));
+webhooksClient.SubscribeAsync(new SubscriptionRequest()
+{
+    Events = new List<string>() { “payment_intent.status_updated” },
+    Url = new Uri(“https://example.com/incoming-events”)
+});
