8d6dcfd2cd522e7da794e4ad1d6cafdebb22080b
myafka <sorry88@bk.ru>
2022-05-10 14:24:36 +0100

    fixed quotation marks

--- webhooks/cs/enable-webhooks.cs
+++ webhooks/cs/enable-webhooks.cs
@@ -1,6 +1,6 @@
-var webhooksClient = new Dojo.Net.WebhooksClient(new HttpClient(), new ApiKeyClientAuthorization(“sk_sandbox_c8oLGaI__msxsXbpBDpdtwJEz_eIhfQoKHmedqgZPCdBx59zpKZLSk8OPLT0cZolbeuYJSBvzDVVsYvtpo5RkQ”));
+var webhooksClient = new Dojo.Net.WebhooksClient(new HttpClient(), new ApiKeyClientAuthorization("sk_sandbox_c8oLGaI__msxsXbpBDpdtwJEz_eIhfQoKHmedqgZPCdBx59zpKZLSk8OPLT0cZolbeuYJSBvzDVVsYvtpo5RkQ"));
 webhooksClient.SubscribeAsync(new SubscriptionRequest()
 {
-    Events = new List<string>() { “payment_intent.status_updated” },
-    Url = new Uri(“https://example.com/incoming-events”)
+    Events = new List<string>() { "payment_intent.status_updated" },
+    Url = new Uri("https://example.com/incoming-events")
 });
