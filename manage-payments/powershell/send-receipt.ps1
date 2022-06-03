b2158e5185f0d11c9e0f49e74bf7cd74331cd19f
Roman Kondratyev <roman.kondratyev@paymentsense.com>
2022-06-03 10:59:28 +0200

    Added PS samples

---
+++ manage-payments/powershell/send-receipt.ps1
@@ -0,0 +1,13 @@
+# This is a public sandbox API key.
+# Don’t submit any personally identifiable information in any requests made with this key.
+# Sign in to developer.dojo.tech to create your own private sandbox key and use that instead
+# for secure testing.
+$publicSandboxKey = "sk_sandbox_c8oLGaI__msxsXbpBDpdtwJEz_eIhfQoKHmedqgZPCdBx59zpKZLSk8OPLT0cZolbeuYJSBvzDVVsYvtpo5RkQ"
+
+# Replace <PAYMENT_INTENT_ID> with ID of payment intent you want to send receipt for.
+Invoke-WebRequest `
+    -Uri 'https://api.dojo.tech/payment-intents/<PAYMENT_INTENT_ID>/receipt' `
+    -Method POST `
+    -Headers @{ "version" = "2022-04-07"; "Authorization" = "Basic $publicSandboxKey" } `
+    -ContentType 'application/json' `
+    -Body '{ "emails": [ "gabriel@dojo.com", "mari@dojo.com" ] }'
