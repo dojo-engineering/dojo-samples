d7efeea05269df20a81438eb8cade59b1f6c0e6b
Monica <94380874+monica-m-ps@users.noreply.github.com>
2022-07-14 11:30:15 +0100

    Updated create payment intent code and deleted payment intent auto cs page

--- getting-started/cs/create-payment-intent.cs
+++ getting-started/cs/create-payment-intent.cs
@@ -3,14 +3,9 @@ var result = await paymentIntentsClient.CreatePaymentIntentAsync(new CreatePayme
 {
     Amount = new()
     {
-        Value = checkoutRequest.Amount,
+        Value = 1000,
         CurrencyCode = "GBP"
     },
-    Config = new()
-    {
-        CancelUrl = new Uri(checkoutRequest.CancelUrl),
-        RedirectUrl = new Uri(checkoutRequest.RedirectUrl),
-    },
-    Description = checkoutRequest.Description,
+    Description = "Demo payment intent",
     Reference = "Order - 1"
 });
