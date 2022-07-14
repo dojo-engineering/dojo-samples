f65ed6097062105e84b0c9957dcad3c4dce1bf9a
Monica <94380874+monica-m-ps@users.noreply.github.com>
2022-07-14 13:31:31 +0100

    Rmcx 244 payment intent code samples (#11)
    * Added payment intent code samples
    
    * incorporated comments and added powershell commands
    
    * Incorporated comments and rectified cs code
    
    * Added conn.close to python code, updated date in header, tested code again
    
    * Removed checkoutRequest usages in cs
    
    * Updated create payment intent code and deleted payment intent auto cs page

---
+++ payment-intent/curl/payment-intent-auto.sh
@@ -0,0 +1,8 @@
+curl -v --request POST \
+    --url https://api.dojo.tech/payment-intents \
+    --header 'content-type: application/json' \
+    --header 'Authorization: Basic sk_sandbox_c8oLGaI__msxsXbpBDpdtwJEz_eIhfQoKHmedqgZPCdBx59zpKZLSk8OPLT0cZolbeuYJSBvzDVVsYvtpo5RkQ' \
+    --header 'version: 2022-07-04' \
+    --data '{"amount":{"value":1000,"currencyCode":"GBP"},
+                "reference":"Order-0001"
+                }'
---
+++ payment-intent/curl/payment-intent-manual.sh
@@ -0,0 +1,9 @@
+curl -v --request POST \
+    --url https://api.dojo.tech/payment-intents \
+    --header 'content-type: application/json' \
+    --header 'Authorization: Basic sk_sandbox_c8oLGaI__msxsXbpBDpdtwJEz_eIhfQoKHmedqgZPCdBx59zpKZLSk8OPLT0cZolbeuYJSBvzDVVsYvtpo5RkQ' \
+    --header 'version: 2022-07-04' \
+    --data '{"captureMode":"Manual",
+            "amount":{"value":1000,"currencyCode":"GBP"},
+            "reference":"Order-0001"
+            }'
