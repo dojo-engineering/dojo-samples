1d8217211699183ec26208e764e23c9f5fbae495
Monica <94380874+monica-m-ps@users.noreply.github.com>
2023-01-12 16:56:18 +0000

    Added comment to all files about sandbox api key

--- manage-payments/python/change-tips-amount.py
+++ manage-payments/python/change-tips-amount.py
@@ -1,3 +1,8 @@
+# The sandbox API key passed in 'authorization' is public.
+# Don't submit any personally identifiable information in any requests made with this key.
+# Sign in to developer.dojo.tech to create your own private sandbox key and use that instead
+# for secure testing.
+
 import http.client
 
 conn = http.client.HTTPSConnection("api.dojo.tech")
--- webhooks/powershell/enable-webhooks.ps1
+++ webhooks/powershell/enable-webhooks.ps1
@@ -2,6 +2,7 @@
 # Don’t submit any personally identifiable information in any requests made with this key.
 # Sign in to developer.dojo.tech to create your own private sandbox key and use that instead
 # for secure testing.
+
 $publicSandboxKey = "sk_sandbox_c8oLGaI__msxsXbpBDpdtwJEz_eIhfQoKHmedqgZPCdBx59zpKZLSk8OPLT0cZolbeuYJSBvzDVVsYvtpo5RkQ"
 Invoke-WebRequest `
     -Uri 'https://api.dojo.tech/webhooks' `
