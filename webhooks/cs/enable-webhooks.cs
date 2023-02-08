8278b20497dae2a3c379a8b5c7677b7a2fad9116
Monica <94380874+monica-m-ps@users.noreply.github.com>
2023-02-08 09:55:02 +0000

    Fixed comment to start with //

--- webhooks/cs/enable-webhooks.cs
+++ webhooks/cs/enable-webhooks.cs
@@ -1,7 +1,7 @@
-/*The sandbox API key passed in 'ApiKeyClientAuthorization' is public.
-Don’t submit any personally identifiable information in any requests made with this key.
-Sign in to developer.dojo.tech to create your own private sandbox key and use that instead
-for secure testing. */
+// The sandbox API key passed in 'ApiKeyClientAuthorization' is public.
+// Don’t submit any personally identifiable information in any requests made with this key.
+// Sign in to developer.dojo.tech to create your own private sandbox key and use that instead
+// for secure testing.
 
 var webhooksClient = new Dojo.Net.WebhooksClient(new HttpClient(), new ApiKeyClientAuthorization("sk_sandbox_c8oLGaI__msxsXbpBDpdtwJEz_eIhfQoKHmedqgZPCdBx59zpKZLSk8OPLT0cZolbeuYJSBvzDVVsYvtpo5RkQ"));
 webhooksClient.SubscribeAsync(new SubscriptionRequest()
