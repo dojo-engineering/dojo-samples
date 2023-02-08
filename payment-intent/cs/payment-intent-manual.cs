946c06314001cccfba2e715c4bc4942d53ddae77
Monica <94380874+monica-m-ps@users.noreply.github.com>
2023-02-08 14:51:16 +0000

    Fixed comment to start with // (#23)

--- payment-intent/cs/payment-intent-manual.cs
+++ payment-intent/cs/payment-intent-manual.cs
@@ -1,7 +1,7 @@
 // The sandbox API key passed in 'ApiKeyClientAuthorization' is public.
 // Don’t submit any personally identifiable information in any requests made with this key.
 // Sign in to developer.dojo.tech to create your own private sandbox key and use that instead
-// for secure testing. */
+// for secure testing.
 
 var paymentIntentsClient = new PaymentIntentsClient(new HttpClient(), new ApiKeyClientAuthorization("sk_sandbox_c8oLGaI__msxsXbpBDpdtwJEz_eIhfQoKHmedqgZPCdBx59zpKZLSk8OPLT0cZolbeuYJSBvzDVVsYvtpo5RkQ"));
 var result = await paymentIntentsClient.CreatePaymentIntentAsync(new CreatePaymentIntentRequest
