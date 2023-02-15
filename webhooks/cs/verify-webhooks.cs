33efc2379f804ee05004f91dfd575df4212c8964
Roman Kondratyev <85743312+RomanKondratyev@users.noreply.github.com>
2023-02-15 13:57:39 +0000

    Added more description to the example (#25)
    Co-authored-by: Roman Kondratyev <roman.kondratyev@dojo.tech>

--- webhooks/cs/verify-webhooks.cs
+++ webhooks/cs/verify-webhooks.cs
@@ -1,11 +1,16 @@
-// read request body as a byte stream
+// This example shows how to do this for Microsoft ASP.NET MVC project (https://learn.microsoft.com/en-us/aspnet/mvc/).
+// However just, reading the HTTP request body of the webhook call as UTF-8 string and 
+// signature header ('dojo-signature') value to compare with would work.
+
+// Read request body as a byte stream
 using var streamReader = new StreamReader(HttpContext.Request.Body);
-var body = await streamReader.ReadToEndAsync();
+string body = await streamReader.ReadToEndAsync();
 
 // Get signature header value
 if (HttpContext.Request.Headers.TryGetValue(WebhookPayloadUtils.SignatureHeaderName, out var signatureHeader))
 {
-    // use Dojo.Net SDK to deserialize and validate webhook payload
+    // Use Dojo.Net SDK to deserialize and validate webhook payload
+    // _webhookSecret is the webhook secret value, created in the Developer portal (i.e. developer.dojo.tech)
     var payload = WebhookPayloadUtils.ReadPayload(body, _webhookSecret, signatureHeader);
 
     // TODO: update your database or do some action based on successful payment
