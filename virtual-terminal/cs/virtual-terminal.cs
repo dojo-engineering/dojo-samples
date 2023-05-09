71c7450cc88dcace3a7e3063d95b687aa835db0a
Monica <94380874+monica-m-ps@users.noreply.github.com>
2023-05-09 15:46:25 +0100

    Task/rmcx 2382 rewrite c sharp code (#28)
    * Added code samples for virtual terminal
    
    * Renamed file
    
    * Removed description. No longer needed
    
    * Tested
    
    * Removed description
    
    * Added c# code to maintain consistency with Python
    
    * Added comment
    
    * removed auto

--- virtual-terminal/cs/virtual-terminal.cs
+++ virtual-terminal/cs/virtual-terminal.cs
@@ -1,9 +1,7 @@
 // The sandbox API key passed in 'ApiKeyClientAuthorization' is public.
-// Don't submit any personally identifiable information in any requests made with this key.
+// Don’t submit any personally identifiable information in any requests made with this key.
 // Sign in to developer.dojo.tech to create your own private sandbox key and use that instead
-// for secure testing.
-
-using Dojo.Net;
+// for secure testing. */
 
 var paymentIntentsClient = new PaymentIntentsClient(new HttpClient(), new ApiKeyClientAuthorization("sk_sandbox_c8oLGaI__msxsXbpBDpdtwJEz_eIhfQoKHmedqgZPCdBx59zpKZLSk8OPLT0cZolbeuYJSBvzDVVsYvtpo5RkQ"));
 var result = await paymentIntentsClient.CreatePaymentIntentAsync(new CreatePaymentIntentRequest
@@ -13,5 +11,6 @@ var result = await paymentIntentsClient.CreatePaymentIntentAsync(new CreatePayme
         Value = 1000,
         CurrencyCode = "GBP"
     },
-    Reference = "Order 245"
+    Reference = "Order 234",
+    TransactionSource = TransactionSource.MOTO
 });
