6244c25f1e17d7d5334c67f41147e45cd9d5de8e
Monica <94380874+monica-m-ps@users.noreply.github.com>
2023-05-19 17:06:41 +0100

    Rectified date and added comment

--- customer-secret/curl/create-customer-secret.sh
+++ customer-secret/curl/create-customer-secret.sh
@@ -1,5 +1,10 @@
+# The sandbox API key passed in 'authorization' is public.
+# Don't submit any personally identifiable information in any requests made with this key.
+# Sign in to developer.dojo.tech to create your own private sandbox key and use that instead
+# for secure testing.
+
 curl -v --request POST \
   --url https://api.dojo.tech/customers/cust_sandbox_6g-HvPv6VkG_Q_PXCpJqmw/create-secret \
   --header 'content-type: application/json' \
   --header 'Authorization: Basic sk_sandbox_c8oLGaI__msxsXbpBDpdtwJEz_eIhfQoKHmedqgZPCdBx59zpKZLSk8OPLT0cZolbeuYJSBvzDVVsYvtpo5RkQ' \
-  --header 'version: 2023-01-10'
+  --header 'version: 2022-04-07'
