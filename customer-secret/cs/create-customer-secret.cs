45275f154d36dcc634f9e039194ab22fefb2d306
Monica <94380874+monica-m-ps@users.noreply.github.com>
2023-05-22 10:16:26 +0100

    Task/rmcx 2491 added code comments and rectified date (#30)
    * Rectified date and added comment
    
    * rectified date and added an extra space after comments
    
    * Added space in the beginning
    
    * Rectified date

--- customer-secret/cs/create-customer-secret.cs
+++ customer-secret/cs/create-customer-secret.cs
@@ -1,4 +1,3 @@
-
 // The sandbox API key passed in 'ApiKeyClientAuthorization' is public.
 // Don't submit any personally identifiable information in any requests made with this key.
 // Sign in to developer.dojo.tech to create your own private sandbox key and use that instead
