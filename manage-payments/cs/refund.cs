91d2253c9b5b3321aacb60d894fdeff8fe7a2a8f
Roman Kondratyev <roman.kondratyev@dojo.tech>
2023-05-22 13:33:03 +0100

    Added & tested each PHP example

--- manage-payments/cs/refund.cs
+++ manage-payments/cs/refund.cs
@@ -4,4 +4,4 @@
 // for secure testing.
 
 var refundsClient = new RefundsClient(new HttpClient(), new ApiKeyClientAuthorization("sk_sandbox_c8oLGaI__msxsXbpBDpdtwJEz_eIhfQoKHmedqgZPCdBx59zpKZLSk8OPLT0cZolbeuYJSBvzDVVsYvtpo5RkQ"));
-refundsClient.CreateAsync("<PAYMENT_INTENT_ID>", "656565gfyd65", new CreateRefundRequest(){Amount = 50, Notes = "Demo refund", RefundReason= "Duplicate transaction" }});
+refundsClient.CreateAsync("<PAYMENT_INTENT_ID>", "656565gfyd65", new CreateRefundRequest() { Amount = 50, Notes = "Demo refund", RefundReason = "Duplicate transaction" });
