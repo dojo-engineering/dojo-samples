ce09fbd4f5debf9c324f91010ea0f714c622fcea
Monica <94380874+monica-m-ps@users.noreply.github.com>
2023-07-06 14:52:02 +0100

    Added React Native code samples

---
+++ mobile-sdk/react-native/payment-intent/react-native/capture-apple-pay.js
@@ -0,0 +1,7 @@
+import { startPaymentFlow } from "@dojo-engineering/react-native-pay-sdk";
+
+const result = await startPaymentFlow({ 
+  intentId: "<paymentIntentId>",
+  applePayMerchantId:"<Merchant_Identifier>" });
+
+console.log(result);
