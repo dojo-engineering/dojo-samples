f9919fbe409563eaad0602d4279e19c680f945b5
Monica <94380874+monica-m-ps@users.noreply.github.com>
2023-07-07 12:42:51 +0100

    Formatted code indentation

--- mobile-sdk/react-native/payment-intent/react-native/capture-apple-pay.js
+++ mobile-sdk/react-native/payment-intent/react-native/capture-apple-pay.js
@@ -1,7 +1,8 @@
 import { startPaymentFlow } from "@dojo-engineering/react-native-pay-sdk";
 
-const result = await startPaymentFlow({ 
+const result = await startPaymentFlow({
   intentId: "<paymentIntentId>",
-  applePayMerchantId:"<Merchant_Identifier>" });
+  applePayMerchantId: "<Merchant_Identifier>"
+});
 
 console.log(result);
--- mobile-sdk/react-native/payment-intent/react-native/capture-google-pay.js
+++ mobile-sdk/react-native/payment-intent/react-native/capture-google-pay.js
@@ -1,9 +1,10 @@
 import { startPaymentFlow } from "@dojo-engineering/react-native-pay-sdk";
 
-const result = await startPaymentFlow({ 
+const result = await startPaymentFlow({
   intentId: "<paymentIntentId>",
-  gPayMerchantId:"<Merchant_Id>",
+  gPayMerchantId: "<Merchant_Id>",
   gPayGatewayMerchantId: "<Gateway_Merchant_Id>",
-  gPayMerchantName: "<Merchant_Name>" });
+  gPayMerchantName: "<Merchant_Name>"
+});
 
 console.log(result);
