3bb08aa12fddf134930e2bd65dcb027a638e6931
Monica <94380874+monica-m-ps@users.noreply.github.com>
2023-10-23 00:23:14 +0100

    Tested and added code for creating payment intent with setup intent ID

---
+++ setup-intent/cs/create-payment-intent.cs
@@ -0,0 +1,17 @@
+// The sandbox API key passed in 'ApiKeyClientAuthorization' is public.
+// Don't submit any personally identifiable information in any requests made with this key.
+// Sign in to developer.dojo.tech to create your own private sandbox key and use that instead
+// for secure testing.
+
+var paymentIntentsClient = new PaymentIntentsClient(new HttpClient(), new ApiKeyClientAuthorization("sk_sandbox_c8oLGaI__msxsXbpBDpdtwJEz_eIhfQoKHmedqgZPCdBx59zpKZLSk8OPLT0cZolbeuYJSBvzDVVsYvtpo5RkQ"));
+var result = await paymentIntentsClient.CreatePaymentIntentAsync(new CreatePaymentIntentRequest
+{
+    Amount = new()
+    {
+        Value = 1000,
+        CurrencyCode = "GBP"
+    },
+    Description = "Demo payment intent",
+    Reference = "Table no.4.",
+    SetupIntentId = "si_<secret-key>",
+});
