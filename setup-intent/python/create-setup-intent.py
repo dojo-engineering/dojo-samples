ba75a7027e8b6dfb4412ff0c12db9cc3737b702c
Monica <monica.manghnani@dojo.tech>
2024-03-18 11:06:04 +0000

    Fixed setup-intent api url

--- setup-intent/python/create-setup-intent.py
+++ setup-intent/python/create-setup-intent.py
@@ -21,7 +21,7 @@ headers = {
     'Version': "2022-04-07",
     'Authorization': "Basic sk_sandbox_c8oLGaI__msxsXbpBDpdtwJEz_eIhfQoKHmedqgZPCdBx59zpKZLSk8OPLT0cZolbeuYJSBvzDVVsYvtpo5RkQ" # <-- Change to your secret key
 }
-conn.request("POST", "/payment-intents", payload, headers)
+conn.request("POST", "/setup-intents", payload, headers)
 res = conn.getresponse()
 data = res.read()
 print(data.decode("utf-8"))
