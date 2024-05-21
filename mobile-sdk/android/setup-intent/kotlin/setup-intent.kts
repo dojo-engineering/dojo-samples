c088ea08464e6287f4aa52d778bb3895a12118eb
Monica <monica.manghnani@dojo.tech>
2024-05-21 14:22:29 +0100

    Android setup intent code

---
+++ mobile-sdk/android/setup-intent/kotlin/setup-intent.kts
@@ -0,0 +1,14 @@
+val dojoHandler = DojoSDKDropInUI.createUIPaymentHandler(this) {
+    // handle result
+    Toast.makeText(this, it.name, Toast.LENGTH_SHORT).show()
+}
+    // on pay tapped
+    button.setOnClickListener {
+        dojoHandler.startPaymentFlow(
+            DojoPaymentFlowParams(
+                paymentId = "pi_sandbox_mDvzElFkoU2QH440cwoOEg",
+                GPayConfig = null, // add this if your app supports Google Pay.
+                paymentType = DojoPaymentType.SETUP_INTENT,
+            ),
+        )
+    } 
