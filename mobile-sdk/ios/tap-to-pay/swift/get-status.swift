89909b99990572df4adb72923421135940ed2c75
Aaron Forinton <135028391+aaronforinton-dojo@users.noreply.github.com>
2024-07-23 14:05:42 +0100

    Update mobile-sdk/ios/tap-to-pay/swift/get-status.swift
    Co-authored-by: Monica <monica.manghnani@paymentsense.com>

--- mobile-sdk/ios/tap-to-pay/swift/get-status.swift
+++ mobile-sdk/ios/tap-to-pay/swift/get-status.swift
@@ -1,6 +1,6 @@
 var dojoSDK: DojoTapToPayOniPhone? = DojoTapToPayOniPhone(env: .staging)
 var terminalStatus: DojoTerminalStatus?
-let secret: String = "secret"
+let secret: String = "<secret>"
 do {
     terminalStatus = try await dojoSDK?.getTerminalActivationStatus(secret: secret)
     } catch {
