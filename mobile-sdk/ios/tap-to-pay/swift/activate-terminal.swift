20204463895a36e10cefd1127871b8184348d726
Aaron Forinton <135028391+aaronforinton-dojo@users.noreply.github.com>
2024-07-23 14:05:48 +0100

    Update mobile-sdk/ios/tap-to-pay/swift/activate-terminal.swift
    Co-authored-by: Monica <monica.manghnani@paymentsense.com>

--- mobile-sdk/ios/tap-to-pay/swift/activate-terminal.swift
+++ mobile-sdk/ios/tap-to-pay/swift/activate-terminal.swift
@@ -1,6 +1,6 @@
 var dojoSDK: DojoTapToPayOniPhone? = DojoTapToPayOniPhone(env: .staging)
 var terminalStatus: DojoTerminalStatus?
-let secret: String = "secret"
+let secret: String = "<secret>"
 do {
     try await dojoSDK?.activateTerminal(secret)
         terminalStatus = try await dojoSDK?.getTerminalActivationStatus(
