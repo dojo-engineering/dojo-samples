cd6de8a2fa6a370490ba284f79458052fb68d476
aaronforinton-dojo <aaronforinton@paymentsense.com>
2024-07-24 11:18:04 +0100

    change quote symbol

--- mobile-sdk/android/tap-to-pay/kotlin/bundle-item.kts
+++ mobile-sdk/android/tap-to-pay/kotlin/bundle-item.kts
@@ -1,13 +1,13 @@
-val baseAmount = bundle.getInt("com.dojo.extra.BASE_AMOUNT”, -1)
-val gratuityAmount = bundle.getInt("com.dojo.extra.GRATUITY_AMOUNT”, -1)
-val txResult = bundle.getString("com.dojo.extra.TRANSACTION_RESULT”)
-val authCode = bundle.getString("com.dojo.extra.AUTH_CODE”)
-val txId = bundle.getString("com.dojo.extra.TRANSACTION_ID”)
-val paymentMethod = bundle.getString("com.dojo.extra.PAYMENT_METHOD”)
+val baseAmount = bundle.getInt("com.dojo.extra.BASE_AMOUNT", -1)
+val gratuityAmount = bundle.getInt("com.dojo.extra.GRATUITY_AMOUNT", -1)
+val txResult = bundle.getString("com.dojo.extra.TRANSACTION_RESULT")
+val authCode = bundle.getString("com.dojo.extra.AUTH_CODE")
+val txId = bundle.getString("com.dojo.extra.TRANSACTION_ID")
+val paymentMethod = bundle.getString("com.dojo.extra.PAYMENT_METHOD")
 
-if (paymentMethod == "KEYED”) return
+if (paymentMethod == "KEYED") return
 
-val cvmString =  bundle.getString("com.dojo.extra.CARDHOLDER_VERIFICATION_METHOD”)
-val cardScheme = bundle.getString("com.dojo.extra.CARD_SCHEME”)
-val appLabel = bundle.getString("com.dojo.extra.CARD_APPLICATION_LABEL”)
-val appId = bundle.getString("com.dojo.extra.CARD_APPLICATION_ID”)
+val cvmString =  bundle.getString("com.dojo.extra.CARDHOLDER_VERIFICATION_METHOD")
+val cardScheme = bundle.getString("com.dojo.extra.CARD_SCHEME")
+val appLabel = bundle.getString("com.dojo.extra.CARD_APPLICATION_LABEL")
+val appId = bundle.getString("com.dojo.extra.CARD_APPLICATION_ID")
